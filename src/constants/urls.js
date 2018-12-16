8417a8c8af2b024e929f5fe50356ee4a67565916
Cristi Cismas <cristicismas99@gmail.com>
2018-12-16 13:07:40 +0200

    Add Yahoo and Bing to the search engines list

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,4 +1,6 @@
 export const URLS = {
   GOOGLE: 'http://google.com/search?q=',
-  DUCKDUCKGO: 'https://duckduckgo.com/?q='
+  DUCKDUCKGO: 'https://duckduckgo.com/?q=',
+  BING: 'https://www.bing.com/search?q=',
+  YAHOO: 'https://us.search.yahoo.com/search?p='
 };
